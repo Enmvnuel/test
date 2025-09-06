0003a40b87fc9c670f109defc35db24baaa6cea6
enmvnuel <cristopherariasp@icloud.com>
2025-09-06 10:27:08 -0500

    Update App component content with placeholder text

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import "./App.css";
 const App = () => {
     return (
         <div className="content">
-            <h1>asdasdasdasdsad</h1>
-            <p>Start building amazing things with Rsbuild.</p>
+            <h1>Test</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Placeat magnam earum, dolor natus aperiam dolorum perferendis
+                officiis harum maiores hic molestiae dolores corporis officia
+                provident soluta esse necessitatibus debitis explicabo.
+            </p>
         </div>
     );
 };
